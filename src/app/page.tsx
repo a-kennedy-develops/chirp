6ca7a546aac2c4d05a0197c41b27580990d92be1
Alex Kennedy <a-kennedy-mac@Alexs-MBP-2.lan>
2025-01-08 17:18:05 -0500

    Add navbar, update meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
-import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ModeToggle";
-
 export default function Login() {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign in</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <ModeToggle />
+      <h1>Homepage content</h1>
     </div>
   );
 }
